747b2835322de8877540f476f906e8873926d000
Ivan Skotar <iskotar.it@gmail.com>
2021-03-04 19:52:34 -0800

    fix Mixed Content issues

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,12 +1,12 @@
 export function httpRequestMovies(params:string) {
-  return fetch(`http://www.omdbapi.com/?apikey=331b7984${params}`)
+  return fetch(`https://www.omdbapi.com/?apikey=331b7984${params}`)
   .then(res => res.json())
   .then(res => res)
   .catch(error => error)
 }
 
 export function httpRequestShows(params:string) {
-  return fetch(`http://api.tvmaze.com/${params}`)
+  return fetch(`https://api.tvmaze.com/${params}`)
   .then(res => res.json())
   .then(res => res)
   .catch(error => error)
