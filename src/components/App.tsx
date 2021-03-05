f38711ca215b8d6ea8ec88289812bcee19b095d6
Ivan Skotar <iskotar.it@gmail.com>
2021-03-04 19:41:26 -0800

    routes fixes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,45 +1,47 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import 'antd/dist/antd.css'
-import { Container } from '@material-ui/core'
-import { Layout, Row } from 'antd'
+import {Container} from '@material-ui/core'
+import {Layout, Row} from 'antd'
 import Search from './Search'
 import MoviesList from './movies/MoviesList'
 import HeaderPanel from './HeaderPanel'
 import MoviesListItemPage from './movies/MoviesListItemPage'
-import { Slider } from './Slider'
+import {Slider} from './Slider'
 import ShowsList from "./shows/ShowsList";
 import ShowsListItemPage from "./shows/ShowsListItemPage";
 
-const { Content, Footer } = Layout
+const {Content, Footer} = Layout
 const BASE_URL = process.env.PUBLIC_URL;
 
 
-function App () {
+function App() {
 
   return (
     <Router basename={BASE_URL}>
       <div className="App">
         <Layout>
           <HeaderPanel/>
-          <Content style={{ background: 'white', minHeight: 'calc(100vh - 55px)' }}>
+          <Content style={{background: 'white', minHeight: 'calc(100vh - 55px)'}}>
             <Slider/>
             <Container>
-              <Row justify={'center'} style={{display: 'grid'}}>
-                <Search/>
-                <Route exact path="/movies">
-                  <MoviesList/>
-                </Route>
-                <Route exact path="/shows">
-                  <ShowsList/>
-                </Route>
-              </Row>
-              <Route path="/movies/:id">
-                <MoviesListItemPage/>
-              </Route>
-              <Route path="/shows/:id">
-                <ShowsListItemPage/>
-              </Route>
+              <Switch>
+                <Row justify={'center'} style={{display: 'grid'}}>
+                  <Search/>
+                  <Route exact path="/movies">
+                    <MoviesList/>
+                  </Route>
+                  <Route exact path="/shows">
+                    <ShowsList/>
+                  </Route>
+                  <Route path="/movies/:id">
+                    <MoviesListItemPage/>
+                  </Route>
+                  <Route path="/shows/:id">
+                    <ShowsListItemPage/>
+                  </Route>
+                </Row>
+              </Switch>
             </Container>
           </Content>
           <Footer>Movie ©2021 Created by Ivan Skotar</Footer>
--- src/components/HeaderPanel.tsx
+++ src/components/HeaderPanel.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Row, Layout } from 'antd'
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo.svg'
 import logo3 from '../assets/shows-logo.svg'
 
@@ -18,12 +19,12 @@ function HeaderPanel () {
           backgroundColor: 'rgb(22 18 39)',
           color: 'white'
         }}>
-        <a href="/movies">
+        <Link to="/movies">
           <img src={logo} alt="" height={'73vw'}/>
-        </a>
-        <a href="/shows">
+        </Link>
+        <Link to="/shows">
           <img src={logo3} alt="" height={'73vw'}/>
-        </a>
+        </Link>
       </Row>
     </Header>
   )
